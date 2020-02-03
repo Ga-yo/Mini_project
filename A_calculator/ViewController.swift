114f197547db73396e7384b68f512805a7cb907d
Ga-yo <49550838+Ga-yo@users.noreply.github.com>
2020-02-04 00:58:11 +0900

    Initial Commit

---
+++ A_calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  A_calculator
+//
+//  Created by 이가영 on 2020/02/04.
+//  Copyright © 2020 이가영. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
