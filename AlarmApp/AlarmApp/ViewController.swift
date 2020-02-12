59284633a99226af2133af62b1f412b7d63f91d2
Ga-yo <49550838+Ga-yo@users.noreply.github.com>
2020-02-12 17:42:54 +0900

    Alram

--- AlarmApp/AlarmApp/ViewController.swift
+++ AlarmApp/AlarmApp/ViewController.swift
@@ -12,33 +12,50 @@ class ViewController: UIViewController {
     let timeSelector : Selector = #selector(ViewController.updateTime)
     let interval = 1.0
     var count = 0
+    var isclickconfirm = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+        
     }
     
     @IBOutlet var current: UILabel!
     @IBOutlet var choose: UILabel!
     
     @IBAction func changedatePicker(_ sender: UIDatePicker){
-        let datepicker = sender
-        
         let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy - mm - dd HH:MM:SS: EEE"
-        choose.text = "선택 시간 : " + formatter.string(from: datepicker.date)
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        choose.text = formatter.string(from: sender.date)
     }
 
     @objc func updateTime(){
-        
-        let date = NSDate()
-        
+
         let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        current.text = formatter.string(from: Date())
         
-        formatter.dateFormat = "yyyy - mm - dd HH:MM:SS: EEE"
+        if isclickconfirm {
+            return
+        }
         
-        current.text = "현재 시간 : " + formatter.string(from: date as Date)
+        if choose.text == current.text {
+            self.isclickconfirm = true
+            
+            Timer.scheduledTimer(timeInterval: 60.0, target: self, selector: #selector(self.timerOn), userInfo: nil, repeats: false)
+            // 60초 뒤에 실행
+            
+            let alert = UIAlertController(title: "알림", message: "설정된 시간입니다!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "확인", style: UIAlertAction.Style.default) { UIAlertAction in self.isclickconfirm = true
+                Timer.scheduledTimer(timeInterval: 60.0, target: self, selector: #selector(self.timerOn), userInfo: nil, repeats: false)
+                })
+                self.present(alert, animated: true, completion: nil)
+        }
     }
+    
+    @objc func timerOn() {
+           isclickconfirm = false // 초기화
+       }
 }
 
