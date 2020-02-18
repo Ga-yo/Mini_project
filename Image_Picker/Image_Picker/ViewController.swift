67a1f6648d3c9820f9e0ae253fe870e8e6e82a3a
Ga-yo <49550838+Ga-yo@users.noreply.github.com>
2020-02-18 20:45:18 +0900

    New!!!

--- Image_Picker/Image_Picker/ViewController.swift
+++ Image_Picker/Image_Picker/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBOutlet weak var pickerView: UIPickerView!
     @IBOutlet weak var textlabel: UILabel!
     @IBOutlet weak var imageView:UIImageView!
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -30,6 +31,17 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return Array[row]
     }
+        
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView{
+        let image = UIImage(named: Array[row])
+        let imageView = UIImageView(image: image)
+        imageView.frame = CGRect(x:0, y:0, width: 100, height: 80)
+        return imageView
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+           return 80
+       }
     
    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
            return 1
