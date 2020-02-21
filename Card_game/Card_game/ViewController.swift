ac5908312d39b44edee25ba757ed6f303e0d160c
Ga-yo <49550838+Ga-yo@users.noreply.github.com>
2020-02-21 21:53:38 +0900

    TIL

--- Card_game/Card_game/ViewController.swift
+++ Card_game/Card_game/ViewController.swift
@@ -42,15 +42,14 @@ class ViewController: UIViewController {
         }
     }
     
-    func emoji(for card : Card) -> String{
-        if emoji[card.identifier] == nil, emojiChoices.count > 0{
+    func emoji(for card: Card) -> String {
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
             let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
             emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
-            
         }
         return emoji[card.identifier] ?? "?"
     }
-    
+     
     @IBOutlet var cardarray: [UIButton]!
     
     override func viewDidLoad() {
