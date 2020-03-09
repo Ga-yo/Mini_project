2b1cff1a1818b16f84c1b5ac2d9082d0ac751a15
Ga-yo <49550838+Ga-yo@users.noreply.github.com>
2020-03-09 20:35:29 +0900

    TIL

--- 2_stopWatch/2_stopWatch/ViewController.swift
+++ 2_stopWatch/2_stopWatch/ViewController.swift
@@ -8,35 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class customCell: UITableViewCell {
+    @IBOutlet weak var tableViewCell: UILabel!
+    
+    override func prepareForReuse() { super.prepareForReuse()
+        tableViewCell.text = "\(lapTime)"
+    }
+
+}
+
+let mainTime: structTime = structTime()
+let lapTime: structTime = structTime()
+var isPlay: Bool = false
+var laps: [String] = []
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var tableview: UITableView!
 
-    let mainTime: structTime = structTime()
-    var isPlay: Bool = false
-
+    var cellIdentifier = "Cell"
     override func viewDidLoad() {
     super.viewDidLoad()
 
+        tableview.delegate = self
+        tableview.dataSource = self
     }
 
-    @IBAction func startButton(_ sender: Any) {
+    @IBAction func startButton(_ sender: AnyObject) {
+
         if !isPlay {
             //Start 버튼이면
             mainTime.time = Timer.scheduledTimer(timeInterval: 0.035, target: self, selector: #selector(UpdateTimer), userInfo: nil, repeats: true)
+            lapTime.time = Timer.scheduledTimer(timeInterval: 0.035, target: self, selector: #selector(UpdateTimer), userInfo: nil, repeats: true)
             isPlay = true
             changeButton(startButton, title: "Stop", color: UIColor.blue)
         } else {
             //Stop 버튼이면
             startButton.isEnabled = true
             mainTime.time.invalidate()
+            lapTime.time.invalidate()
             isPlay = false
             changeButton(startButton, title: "Start", color: UIColor.black)
         }
     }
 
-    @IBAction func resetButton(_ sender: Any) {
+    @IBAction func resetButton(_ sender: AnyObject) {
         mainTime.time.invalidate()
         mainTime.count = 0.0
         resetButton.isEnabled = true
@@ -51,6 +69,18 @@ class ViewController: UIViewController {
     @objc func UpdateTimer() {
         mainTime.count = mainTime.count + 0.035
         timeLabel.text = String(format: "%.2f", mainTime.count)
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: customCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! customCell
+        let text: String = "\(lapTime.time)"
+        cell.tableViewCell?.text = text
+
+        return cell
     }
 
 }
