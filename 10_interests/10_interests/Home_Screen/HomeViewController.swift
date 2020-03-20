9274ea8ccdad2019d9c789f2cf01c7dbca552216
Ga-yo <49550838+Ga-yo@users.noreply.github.com>
2020-03-20 17:08:33 +0900

    10_Interests
    CollectionView를 이용한 앱만들기
    https://www.youtube.com/watch?v=JG7mWFcU0vk&t=226s

---
+++ 10_interests/10_interests/Home_Screen/HomeViewController.swift
@@ -0,0 +1,62 @@
+//
+//  HomeViewController.swift
+//  10_interests
+//
+//  Created by 이가영 on 2020/03/20.
+//  Copyright © 2020 이가영. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    //MARK: - IBOutlets
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+//    @IBOutlet weak var currentUserProfileImageButton: UIButton!
+//    @IBOutlet weak var currentUserFullNameButton: UIButton!
+    
+    //MARK: -UICollectionViewDataSource
+    private var interests = Interest.createInterests()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    private struct Storyboard{
+        static let CellIdentifier = "Cell"
+    }
+}
+
+
+extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelegate{
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int{
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return interests.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Storyboard.CellIdentifier, for: indexPath) as! InterestCollectionViewCell
+        
+        cell.interest = self.interests[(indexPath  as NSIndexPath).item]
+        return cell
+    }
+    
+    
+}
---
+++ 10_interests/10_interests/Home_Screen/InterestCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  InterestCollectionViewCell.swift
+//  10_interests
+//
+//  Created by 이가영 on 2020/03/20.
+//  Copyright © 2020 이가영. All rights reserved.
+//
+
+import UIKit
+
+class InterestCollectionViewCell: UICollectionViewCell {
+    //MARK: Public API
+    var interest: Interest! {
+        didSet{
+            updateUI()
+        }
+    }
+    // MARK: - Private
+    @IBOutlet weak var featuredImageView: UIImageView!
+    @IBOutlet weak var interestTitleLabel: UILabel!
+    
+    func updateUI(){
+        interestTitleLabel?.text = interest.title
+        featuredImageView?.image = interest.featuredImage
+    }
+}
