aeed5f210306daf2f79854e4321b272f87bca6a5
Ga-yo <49550838+Ga-yo@users.noreply.github.com>
2020-04-02 14:26:47 +0900

    데이터모델_정의_성공

--- FoodTracker/FoodTracker/Meal.swift
+++ FoodTracker/FoodTracker/Meal.swift
@@ -12,8 +12,14 @@ class Meal{
     var name: String
     var photo: UIImage?
     var rating: Int
+    
     init?(name: String, photo: UIImage?, rating: Int) {
-        if(name.isEmpty || rating < 0){
+        guard !name.isEmpty else {
+            return nil
+        }
+         
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
             return nil
         }
         self.name = name
---
+++ FoodTracker/FoodTrackerTests/FoodTrackerTests.swift
@@ -0,0 +1,54 @@
+//
+//  FoodTrackerTests.swift
+//  FoodTrackerTests
+//
+//  Created by 이가영 on 2020/04/02.
+//  Copyright © 2020 이가영. All rights reserved.
+//
+
+import XCTest
+@testable import FoodTracker
+
+class FoodTrackerTests: XCTestCase {
+
+    func testMealInitializationSucceeds(){
+        let zeroRatingMeal = Meal.init(name: "Zero", photo: nil, rating: 0)
+        XCTAssertNotNil(zeroRatingMeal)
+         
+        // Highest positive rating
+        let positiveRatingMeal = Meal.init(name: "Positive", photo: nil, rating: 5)
+        XCTAssertNotNil(positiveRatingMeal)
+    }
+    
+    func testMealInitializationFails() {
+        let negativeRatingMeal = Meal.init(name: "Negative", photo: nil, rating: -1)
+        XCTAssertNil(negativeRatingMeal)
+         
+        // Empty String
+        let emptyStringMeal = Meal.init(name: "", photo: nil, rating: 0)
+        XCTAssertNil(emptyStringMeal)
+        
+        let largeRatingMeal = Meal.init(name: "Large", photo: nil, rating: 6)
+        XCTAssertNil(largeRatingMeal)
+    }
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
