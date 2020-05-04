d7b2c192511cd11018202b43a1803cdd0e6fdbbd
Ga-yo <49550838+Ga-yo@users.noreply.github.com>
2020-05-04 20:04:43 +0900

    계산기 - 주석처리

--- New_Calculator/New_Calculator/ViewController.swift
+++ New_Calculator/New_Calculator/ViewController.swift
@@ -21,13 +21,15 @@ class ViewController: UIViewController {
 
     }
 
-    var didSelect = true
+    
+    //var didSelect = true
+    //숫자가 입력되고 있는지 확인
     var isTyping = false
     
-    var fristN: String! = nil
-    var secondN: String! = nil
-    var resultNum: String = "0"
-    var operations: String = ""
+    var fristN: String! = nil //첫번째 항을 기억할 변수
+    var secondN: String! = nil //두번째 항을 기억할 변수
+    var resultNum: String = "0" //결과를 나타낼 변수
+    var operations: String = "" //연산자를 기억할 변수
     
     @IBAction func pressnum(_ sender: UIButton) {
         //숫자를 누르면 초기화 버튼이 C로 바뀐다.
@@ -44,33 +46,40 @@ class ViewController: UIViewController {
     }
     
     @IBAction func operating(_ sender: UIButton){
+        //연산 버튼을 누르면 첫번째 항의 숫자를 저장한다.
         fristN = resultLabel?.text
         
         resultLabel?.text = ""
         
+        //현재 연산자를 변수에 기억한다.
         operations = (sender.titleLabel?.text)!
         
     }
     
     @IBAction func resetC(_ sender: UIButton){
+        
         if resetAC.titleLabel?.text == "C" {
             resetAC.setTitle("AC", for: .normal)
             resultLabel.text = nil
             isTyping = false
         }
+        
     }
     
     @IBAction func resultCal(_ sender: UIButton){
-        
+        //결과 버튼을 누르면 두번째 항의 숫자를 저장한다.
         secondN = resultLabel?.text
         
+        //연산하는 함수 호출
         resultNum = Calculation(operations, fristN, secondN)
         
+        //결과라벨에 결과를 출력
         resultLabel?.text = resultNum
     }
     
     @IBAction func minusplus(_ sender: UIButton){
         let minus = Int(resultLabel.text!)
+        //만약 지금 숫자가 양수이면 -를 붙이고 음수면 -를 제거한다.
         if minus! > 0{
             resultLabel.text = String(-minus!)
         }else if minus! < 0 {
@@ -79,13 +88,13 @@ class ViewController: UIViewController {
     }
     
     @IBAction func percentage(_ sender: UIButton){
+        //나눠야하기 때문에 double형으로 형변환을 해준다.
         let percent = Double(resultLabel.text!)
         
         resultLabel.text = String(percent!/100)
     }
     
     func Calculation(_ oper: String, _ firstNum: String?, _ secondNum: String?) -> String {
-        
         let result: Double
         let first = Double(firstNum!)
         let second = Double(secondNum!)
