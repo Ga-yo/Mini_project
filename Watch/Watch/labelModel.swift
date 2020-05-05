b63800de80fcbfa51c4f551116d4bc2026cc02f9
Ga-yo <49550838+Ga-yo@users.noreply.github.com>
2020-05-05 18:14:01 +0900

    시계앱 - 전반적인 UI 구성

---
+++ Watch/Watch/labelModel.swift
@@ -0,0 +1,21 @@
+//
+//  labelModel.swift
+//  Watch
+//
+//  Created by 이가영 on 2020/05/05.
+//  Copyright © 2020 이가영. All rights reserved.
+//
+
+import Foundation
+
+class labelModel {
+    
+    var time: String!
+    var country: String!
+    var dayclock: String!
+    
+    init(time: String, country: String) {
+        self.time = time
+        self.country = country
+    }
+}
